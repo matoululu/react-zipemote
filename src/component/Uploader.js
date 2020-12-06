aae1c7747c1f0df0d7b9a18c99c666169e8312e8
Matt <matthew.marineau@gmail.com>
2020-12-06 11:27:22 -0500

    changes

--- src/component/Uploader.js
+++ src/component/Uploader.js
@@ -1,17 +1,29 @@
 import upload from '../images/upload.svg';
+import loading from '../images/loading.gif';
 import React from "react";
 import Dropzone from 'react-dropzone';
+import Resizer from 'react-image-file-resizer';
 
 class Uploader extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {showError:false};
+    this.state = {
+      showError:false,
+      imageResized: false,
+      isInactive: true
+    };
+    this.resizedImageArray = [];
+    this.sizeArray = [28,56,112];
   }
 
-  handleAcceptedDrop = (file) => {
-    const blob = URL.createObjectURL(file[0]);
-    console.log(blob);
+  handleAcceptedDrop = async (file) => {
+    await this.resizeFile(file[0]);
+
+    this.setState(state => ({
+      isInactive: true,
+      imageResized: true
+    }));
   }
 
   handleRejectedDrop = () => {
@@ -20,20 +32,53 @@ class Uploader extends React.Component {
     }));
   }
 
+  resizeFile = (blob) => new Promise(resolve => {
+
+    this.setState(state => ({
+      isInactive: false,
+      imageResized: false
+    }));
+
+    this.resizedImageArray = [];
+
+    this.sizeArray.map((size) => {
+      Resizer.imageFileResizer(blob, size, size, 'PNG', 100, 0,
+      uri => {
+        resolve(this.resizedImageArray.push(uri));
+      },
+      'base64'
+      );
+
+      return true;
+    })
+  });
+
   render() {
     return (
       <main className="container">
         {this.state.showError ? <p className="warning">Image type must be .jpeg or .png</p> : null }
-        <Dropzone onDropAccepted={this.handleAcceptedDrop} onDropRejected={this.handleRejectedDrop} accept="image/jpeg, image/png">
-          {({getRootProps, getInputProps}) => (
-            <div className="uploader" {...getRootProps()}>
-              <input {...getInputProps()} />
-              <img src={upload} className="upload-icon" alt="Upload" />
-              <label>Drag 'n' drop your image file here, or click to select an image</label>
-              <em>(Only *.jpeg and *.png images will be accepted)</em>
-            </div>
-          )}
-        </Dropzone>
+        <div className="uploader">
+        {this.state.isInactive ?
+          <Dropzone onDropAccepted={this.handleAcceptedDrop} onDropRejected={this.handleRejectedDrop} accept="image/jpeg, image/png">
+            {({getRootProps, getInputProps}) => (
+              <div className="uploader__form" {...getRootProps()}>
+                <input {...getInputProps()} />
+                <img src={upload} className="upload-icon" alt="Upload" />
+                <label>Drop your .png or .jpg file or click here!</label>
+              </div>
+            )}
+          </Dropzone>
+          : <div className="uploader"><img src={loading} className="loading-icon" alt="Loading" /></div> }
+        </div>
+
+        {this.state.imageResized ?
+        <ul className="preview">
+          {this.resizedImageArray.map(preview => {
+            return <li key={preview}><img src={preview} alt="preview"/></li>
+          })}
+        </ul>
+        : null }
+
       </main>
     );
   }
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,4 +1,4 @@
-import logo from '../logo.svg';
+import logo from '../images/zipemote.svg';
 
 function Header() {
   return (
