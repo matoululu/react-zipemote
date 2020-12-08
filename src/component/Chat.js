8b7621a3942e6337b4c2eea34279a2321d25785d
Matt <matthew.marineau@gmail.com>
2020-12-08 13:57:37 -0500

    omg to zip

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -6,13 +6,13 @@ function Header(props) {
       <h2>Chat preview</h2>
       <div className="chat__wrap">
         <ul className="chat--light">
-          <li><img src={broadcaster} alt=""/> <span className="name">omgmrm:</span> Your message will look like:</li>
-          <li><img src={broadcaster} alt=""/> <span className="name">omgmrm:</span> <img className="emote" src={props.emote} alt=""/></li>
+          <li><img src={broadcaster} alt=""/> <span className="name">ZipEmote:</span> Your message will look like:</li>
+          <li><img src={broadcaster} alt=""/> <span className="name">ZipEmote:</span> <img className="emote" src={props.emote} alt=""/></li>
         </ul>
 
         <ul className="chat--dark">
-          <li><img src={broadcaster} alt=""/> <span className="name">omgmrm:</span> Your message will look like:</li>
-          <li><img src={broadcaster} alt=""/> <span className="name">omgmrm:</span> <img className="emote" src={props.emote} alt=""/></li>
+          <li><img src={broadcaster} alt=""/> <span className="name">ZipEmote:</span> Your message will look like:</li>
+          <li><img src={broadcaster} alt=""/> <span className="name">ZipEmote:</span> <img className="emote" src={props.emote} alt=""/></li>
         </ul>
       </div>
 
